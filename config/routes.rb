1589a3b3fddbb2b9b579e90fae8b550d97ea2b67
vagrant <vagrant@vagrant.vm>
2018-10-22 18:06:37 +0000

    get root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'productos/index'
-  resources :productos
-  root 'productos#index'
  
+  resources :productos
+
+  get 'productos/index'
+  root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
